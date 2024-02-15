20c29dbfc3e3e1308c018016eb8aae2ee40d327e
mattia_morriale <mattia.morriale1@gmail.com>
2024-02-15 15:55:55 +0100

    minestone 2 fatto

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const containerElement = document.querySelector('#container');
+
 const posts = [
     {
         "id": 1,
@@ -56,34 +58,44 @@ const posts = [
     }
 ];
 
-for (let i = 0; i < posts.length; i++) {
-    console.log(images[i])
+posts.forEach(calogero => {
+    containerElement.innerHTML +=
+    `
+    <div class="post">
+        <div class="post__header">
+            <div class="post-meta">                    
+                <div class="post-meta__icon">
+                    <img class="profile-pic" src="${calogero.author.image}" alt="Phil Mangione">                    
+                </div>
+                <div class="post-meta__data">
+                    <div class="post-meta__author">${calogero.author.name}</div>
+                    <div class="post-meta__time">${calogero.created}</div>
+                </div>                    
+            </div>
+        </div>
+        <div class="post__text">${calogero.content}</div>
+        <div class="post__image">
+            <img src="${calogero.media}" alt="">
+        </div>
+        <div class="post__footer">
+            <div class="likes js-likes">
+                <div class="likes__cta">
+                    <a class="like-button  js-like-button" href="#" data-postid="1">
+                        <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                        <span class="like-button__label">Mi Piace</span>
+                    </a>
+                </div>
+                <div class="likes__counter">
+                    Piace a <b id="like-counter-1" class="js-likes-counter">${calogero.likes}</b> persone
+                </div>
+            </div> 
+        </div>            
+    </div>
+    `
+
+
+})
 
     
-    let text = "";
 
-    let smallimg = "";
-
-    let bigimg = "";
-
-    for (let key in(images[i])) {
-
-        console.log(`${key}: ${images[i][key]}`)
-
-        smallimg += `<img class="img-fluid w-100 h-100  object-fit-cover" src="${images[i][key]}" alt="#">`;
-
-        bigimg += `<img class="img-fluid object-fit-cover w-100 photo" src="${images[i][key]}" alt="#">`;
-        
-        text += `<p>${images[i][key]}<br></p>`;
-        
-        // if (key == 'image') {
-            
-            
-        // } else {
-            
-        // }
-        
-    }
-
-}
 
